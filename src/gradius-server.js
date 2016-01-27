7e99fcf743066306370fcab01c099c8a027e22d0
Luís Portela Afonso <luis.afonso@mindera.com>
2016-01-27 19:08:52 +0000

    Fixed usage mispelled method on gradius-server.js

--- src/gradius-server.js
+++ src/gradius-server.js
@@ -64,7 +64,7 @@ function validateReceivedPacket(packet) {
     logger.info(`validated? ${success}`);
 
     if (success) {
-      radiusServer.sendAccessAcept(packet);
+      radiusServer.sendAccessAccept(packet);
     } else {
       radiusServer.sendAccessReject(packet);
     }
